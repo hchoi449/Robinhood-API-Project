3c0e069f1cce4850b1cbd636c9945f69285d413d
Chang Hyeon Choi <hchoi449@gmail.com>
2023-01-27 16:39:27 -0500

    01/27/2023

---
+++ Nasdaq.py
@@ -0,0 +1,7 @@
+import nasdaqdatalink
+
+mydata = nasdaqdatalink.get_table('ZACKS/FC', ticker='AAPL')
+print(mydata)
+
+##Email: hchoi449@gmail.com
+##pass: Iwp98nga89g#
--- backend.py
+++ backend.py
@@ -1,3 +1,19 @@
-##Python backend!
+import robin_stocks
+from robin_stocks import *
+import robin_stocks.robinhood as r
+import pyotp
 
-##Library used
+## login information
+totp  = pyotp.TOTP("My2factorAppHere").now()
+print("Current OTP", totp)
+
+login = r.login('hchoi449@gmail.com','Iwp98nga89g#', by_sms=True)
+
+## looking at stock portfolio
+my_stocks = r.build_holdings()
+for key,value in my_stocks.items():
+     print(key,value, "\n")
+     
+
+
+
